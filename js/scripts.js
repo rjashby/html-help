3dcfda7d6478db1eeaa766bd74e4b2c97d4b73ce
rjashby <rjapsu160@gmail.com>
2021-10-18 16:11:30 -0700

    WIP ending commit for any changes not pushed.
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a heading.");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  $("img").click(function() {
+    alert("This is an image.");
+  });
 });
